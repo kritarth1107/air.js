013dd0d5f6225fd60b4cde142c025b7ccbab4f41
kritarth1107 <singhalkirtarth@gmail.com>
2024-02-05 13:38:32 +0530

    ADD: sign transactions in cosmos

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 This repository is currently under development and is not ready for production use. Please wait for the official release before using any of its components.
 `
 # Air.js
-Air.js is a TypeScript implementation of the EVM, Cosmwasm & SVM JSON RPC API and related tooling maintained by Airchains Network.
+Air.js is a Javascript implementation of the EVM, Cosmwasm & SVM JSON RPC API and related tooling maintained by Airchains Network.
 
 
--- src/cosmos/index.js
+++ src/cosmos/index.js
@@ -78,7 +78,7 @@ export class Cosmos {
     var buf2 = new Buffer.from([pubKeyByte.length]);
     var buf3 = new Buffer.from(pubKeyByte);
     const pubKey = Buffer.concat([buf1, buf2, buf3]);
-    const pubKeyAny = new message.google.protobuf.Any({
+    const pubKeyAny = new methods.google.protobuf.Any({
       type_url: "/cosmos.crypto.secp256k1.PubKey",
       value: pubKey,
     });
@@ -92,17 +92,69 @@ export class Cosmos {
       "/smart/" +
       toHex(query) +
       "?encoding=UTF-8";
-    const response = await fetch(this.url + smartQueryApi).then((response) => response.json());
+    const response = await fetch(this.url + smartQueryApi).then((response) =>
+      response.json()
+    );
     return response;
   }
 
   async getAccounts(address) {
     let accountsApi = "/cosmos/auth/v1beta1/accounts/";
-    const response =  await fetch(this.url + accountsApi + address).then((response) =>
-      response.json()
+    const response = await fetch(this.url + accountsApi + address).then(
+      (response) => response.json()
     );
 
     return response;
+  }
+
+  sign(txBody, authInfo, accountNumber, privKey) {
+    const bodyBytes = methods.cosmos.tx.v1beta1.TxBody.encode(txBody).finish();
+    const authInfoBytes =
+      methods.cosmos.tx.v1beta1.AuthInfo.encode(authInfo).finish();
+    const signDoc = new methods.cosmos.tx.v1beta1.SignDoc({
+      body_bytes: bodyBytes,
+      auth_info_bytes: authInfoBytes,
+      chain_id: this.chainId,
+      account_number: Number(accountNumber),
+    });
+    let signMessage =
+      methods.cosmos.tx.v1beta1.SignDoc.encode(signDoc).finish();
+    const hash = crypto.createHash("sha256").update(signMessage).digest();
+    const sig = secp256k1.sign(hash, Buffer.from(privKey));
+    const txRaw = new methods.cosmos.tx.v1beta1.TxRaw({
+      body_bytes: bodyBytes,
+      auth_info_bytes: authInfoBytes,
+      signatures: [sig.signature],
+    });
+    const txBytes = methods.cosmos.tx.v1beta1.TxRaw.encode(txRaw).finish();
+    const txBytesBase64 = Buffer.from(txBytes, "binary").toString("base64");
+    return txBytes;
+  }
+
+  // "BROADCAST_MODE_UNSPECIFIED", "BROADCAST_MODE_BLOCK", "BROADCAST_MODE_SYNC", "BROADCAST_MODE_ASYNC"
+  broadcast(signedTxBytes, broadCastMode = "BROADCAST_MODE_SYNC") {
+    const txBytesBase64 = Buffer.from(signedTxBytes, "binary").toString(
+      "base64"
+    );
+
+    var options = {
+      method: "POST",
+      url: this.url + "/cosmos/tx/v1beta1/txs",
+      headers: { "Content-Type": "application/json" },
+      body: { tx_bytes: txBytesBase64, mode: broadCastMode },
+      json: true,
+    };
+
+    return new Promise(function (resolve, reject) {
+      request(options, function (error, response, body) {
+        if (error) return reject(error);
+        try {
+          resolve(body);
+        } catch (e) {
+          reject(e);
+        }
+      });
+    });
   }
 }
 
